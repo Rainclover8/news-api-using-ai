77706ec59f0d96a8deb91d631d79a1b1a0664dcd
merahab <ts.baran6161@gmail.com>
2025-02-28 13:18:28 +0300

    loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Loader from "./components/Loader";
 
 interface NewsItem {
   key: string;
@@ -15,6 +22,7 @@ interface NewsItem {
 
 export default function Home() {
   const [news, setNews] = useState<NewsItem[]>([]);
+  const [loading, setLoading] = useState(true);
   const API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
 
   useEffect(() => {
@@ -29,6 +37,7 @@ export default function Home() {
       .then((data) => {
         if (data.success) {
           setNews(data.result);
+          setLoading(false);
         } else {
           console.error("Haberler alınamadı", data);
         }
@@ -39,27 +48,50 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8">Son Haberler</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {news.map((item) => (
-          <Card key={item.key} className="overflow-hidden shadow-lg">
-            {item.image && (
-              <img src={item.image} alt={item.name} className="w-full h-48 object-cover" />
-            )}
-            <CardHeader>
-              <CardTitle>{item.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-gray-600 text-sm">{item.description}</p>
-            </CardContent>
-            <CardFooter className="flex justify-between items-center text-sm text-gray-500">
-              <span>{new Date(item.date).toLocaleDateString("tr-TR")}</span>
-              <Link href={`/news/${item.key}`} className="text-blue-600 hover:underline">
-                Detayları Oku
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
+      {loading ? (
+        <div className="">
+          <Loader />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {news.map((item) => (
+            <Card key={item.key} className="overflow-hidden shadow-lg">
+              {item.image &&
+                (loading ? (
+                  <div className="">
+                    {" "}
+                    <Loader />
+                  </div>
+                ) : (
+                  <div className="">
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="w-full h-48 object-cover"
+                    />
+                  </div>
+                ))}
+              <CardHeader>
+                <CardTitle>{item.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-gray-600 text-sm">{item.description}</p>
+              </CardContent>
+              <CardFooter className="flex justify-between items-center text-sm text-gray-500">
+                <span>{new Date(item.date).toLocaleDateString("tr-TR")}</span>
+                <Link
+                  href={`/news/${item.key}`}
+                  className="text-blue-600 hover:underline"
+                >
+                  Detayları Oku
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      )}
+      {/* </div> */}
     </main>
   );
 }
