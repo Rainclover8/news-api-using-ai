8ae5f980538f27b1bc21726756ff926e333b5ff3
merahab <ts.baran6161@gmail.com>
2025-03-02 21:08:22 +0300

    class

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-8">Son Haberler</h1>
       {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
       {loading ? (
-        <div className="">
+        <div className="flex justify-center items-center ">
           <Loader />
         </div>
       ) : (
