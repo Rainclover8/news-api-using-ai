7c6126de81e2fd2fc7241293740bee148c362c91
rainclover8 <ts.baran6161@gmail.com>
2025-04-11 12:51:24 +0300

    ads

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
 
+  // Umarım Artık reklam alabiliriz!!!
   useEffect(() => {
     fetch("https://api.collectapi.com/news/getNews?country=tr&tag=general", {
       method: "GET",
