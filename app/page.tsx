9cd4f640e148dfaf8d4ea9ca4bdfc7d5e09d8ea4
rainclover8 <ts.baran6161@gmail.com>
2025-04-21 20:36:26 +0300

    addb

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
     "Güvenilir Kaynaklar",
     "Haberler Burada!",
   ];
-
+// ADS BEKLEME
   return (
     <main className="container mx-auto px-4 py-8 overflow-hidden">
       <MorphingText texts={texts} className="w-full md:text-nowrap mb-12" />
